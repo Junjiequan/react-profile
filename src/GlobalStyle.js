7e0ee39c5481693884c7b3b200fe73fdfb8ac291
mmmG <a331998513@gmail.com>
2021-03-16 20:08:49 +0100

    started

---
+++ src/GlobalStyle.js
@@ -0,0 +1,14 @@
+import {createGlobalStyle} from 'styled-components'
+
+export const Global = createGlobalStyle`
+    *{padding:0;
+      margin:0;
+      box-sizing:border-box;
+    }
+    body{
+      font-family:Arial, Helvetica, sans-serif;
+      font-weight:bold;
+      font-size:5rem;
+    }
+`
+
